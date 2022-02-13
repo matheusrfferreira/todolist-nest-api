87bb1c0305bd24ef324790e1e28644fc635bd4b3
matheusferreira1940@outlook.com <matheusferreira1940@outlook.com>
2022-02-13 18:15:56 -0300

    Initial Commit

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TasksModule } from './tasks/tasks.module';
+
+@Module({
+  imports: [
+    UsersModule,
+    TasksModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://matheus:matheus123@todocluster.5rwwa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    ),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,4 @@
+export class CreateUserDto {
+  user: string;
+  password: string;
+}
